3ee0bae86e72f863c389c8c374bdb64df524b3b4
Zim <zezeozue@google.com>
2022-09-13 15:16:47 +0100

    Clean up binder txn tracking special case
    Previously, binder txns were only traced for txns from sysui/launcher
    to system_server.
    
    ag/Iffc4bb534d239c07d2e4d43ab43a9ed76abe7496 enabled binder txn tracking
    more broadly
    
    Removed the special case for sysui/launcher and cleaned up the code
    
    Test: Manual
    Bug: 161393989
    Change-Id: I9d46d2362ca0ca33bc2477d68092f7ba5fe9c8ac
    Merged-In: I9d46d2362ca0ca33bc2477d68092f7ba5fe9c8ac
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -16372,11 +16372,6 @@ public class ActivityManagerService extends IActivityManager.Stub
         }
     }
 
-    @Override
-    public void enableBinderTracing() {
-        Binder.enableTracingForUid(Binder.getCallingUid());
-    }
-
     @VisibleForTesting
     public final class LocalService extends ActivityManagerInternal
             implements ActivityManagerLocal {
