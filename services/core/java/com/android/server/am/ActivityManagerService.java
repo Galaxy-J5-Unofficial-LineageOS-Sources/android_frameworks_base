a846653065c90ac16c428ddc51b6f67bbeceff31
Yaroslav Zviezda <acroreiser@gmail.com>
2022-11-21 21:27:06 +0300

    Revert "Clean up binder txn tracking special case"
    This reverts commit 89f250f5ee23059bb0c747d308b04cf20d909da8.

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -16372,6 +16372,11 @@ public class ActivityManagerService extends IActivityManager.Stub
         }
     }
 
+    @Override
+    public void enableBinderTracing() {
+        Binder.enableTracingForUid(Binder.getCallingUid());
+    }
+
     @VisibleForTesting
     public final class LocalService extends ActivityManagerInternal
             implements ActivityManagerLocal {
